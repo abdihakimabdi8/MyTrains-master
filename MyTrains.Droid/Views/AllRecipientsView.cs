83ca48c65e4a074c448a1bd18a24e0b1aaa11a60
Hakim Abdi <Hakim.Abdi@VML.com>
2017-09-18 20:33:46 -0500

    Fragment approach implemented for addRecipient and allRecipient functionality

--- MyTrains.Droid/Views/AllRecipientsView.cs
+++ MyTrains.Droid/Views/AllRecipientsView.cs
@@ -1,17 +1,38 @@
 ﻿using Android.App;
 using Android.OS;
 using MvvmCross.Droid.Views;
+using Android.Runtime;
+using Android.Views;
+using MvvmCross.Binding.Droid.BindingContext;
+using MvvmCross.Droid.Shared.Attributes;
+using MvvmCross.Droid.Support.V7.Fragging.Fragments;
+using MyTrains.Core.ViewModel;
+using MyTrains.Droid.Activities;
+using MyTrains.Droid.Extensions;
 
 namespace MyTrains.Droid.Views
 {
-    [Activity(Label = "All Recipients", NoHistory = true)]
-    public class AllRecipientsView : MvxActivity
+    [MvxFragment(typeof(MainViewModel), Resource.Id.content_frame, true)]
+    [Register("mytrains.droid.views.AllRecipientsView")]
+    public class AllRecipientsView : MvxFragment<AllRecipientsViewModel>
+   // [Activity(Label = "All Recipients", NoHistory = true)]
     {
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
-            SetContentView(Resource.Layout.AllRecipientsView);
-        }
+        //protected override void OnCreate(Bundle bundle)
+        //{
+        //    base.OnCreate(bundle);
+        //    SetContentView(Resource.Layout.AllRecipientsView);
+        //}
+        //{
+            public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+            {
+                base.OnCreateView(inflater, container, savedInstanceState);
+                return this.BindingInflate(Resource.Layout.AllRecipientsView, null);
+            }
 
-    }
-}
+            public override void OnViewCreated(View view, Bundle savedInstanceState)
+            {
+                base.OnViewCreated(view, savedInstanceState);
+                (this.Activity as MainActivity).SetCustomTitle("Recipients");
+            }
+        }
+ }
