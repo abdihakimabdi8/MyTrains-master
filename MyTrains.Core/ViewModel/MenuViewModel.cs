995651394f2c7025069567a8d2a8bbbe970eb355
Hakim Abdi <Hakim.Abdi@VML.com>
2017-10-01 15:42:04 -0500

    Changed the fuctionality of the Send Money menu item. It will lead to a view where the user can choose from a data persisted recipient.

--- MyTrains.Core/ViewModel/MenuViewModel.cs
+++ MyTrains.Core/ViewModel/MenuViewModel.cs
@@ -26,8 +26,8 @@ namespace MyTrains.Core.ViewModel
             MenuItems.Add(new MenuItem
             {
                 Title = "Send Money",
-                ViewModelType = typeof(TransferViewModel),
-                Option = MenuOption.CreateTransfer,
+                ViewModelType = typeof(SendViewModel),
+                Option = MenuOption.Send,
                 IsSelected = false
             });
 
